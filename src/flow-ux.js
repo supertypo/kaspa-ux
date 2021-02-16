119d34b48b12957d369f84cb7ed6083c6f7ee9ab
surinder singh <surinder83singh@gmail.com>
2021-02-17 05:02:30 +0530

    /node_modules/@aspectron/flow-ux/  ---->   /flow/flow-ux

--- src/flow-ux.js
+++ src/flow-ux.js
@@ -1,3 +1,9 @@
-export * from '/node_modules/@aspectron/flow-ux/src/flow-format.js';
-export * from '/node_modules/@aspectron/flow-ux/src/base-element.js';
-export * from '/node_modules/@aspectron/flow-ux/src/flow-swipeable.js';
+export * from '/flow/flow-ux/src/flow-format.js';
+export * from '/flow/flow-ux/src/base-element.js';
+export * from '/flow/flow-ux/src/flow-swipeable.js';
+import {paginationStyle as pCss, css} from '/flow/flow-ux/src/base-element.js';
+export const paginationStyle = [pCss, css`
+	.pagination a{
+		padding:var(--flow-btn-padding);
+	}
+`]
