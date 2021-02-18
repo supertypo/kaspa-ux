4d6c0b97a4707b75606d102fbcf0edcd632ead3f
surinder singh <surinder83singh@gmail.com>
2021-02-19 03:39:04 +0530

    OfflineBadge icon issue

--- src/kaspa-wallet-mobile.js
+++ src/kaspa-wallet-mobile.js
@@ -181,7 +181,10 @@ export class KaspaWalletMobile extends KaspaWalletUI{
 					src="${baseUrl+'/resources/images/logo.png'}" />
 			</div>
 			<div class="flex"></div>
-			${this.isOfflineBadge?html`<div class='header-status'>${this.isOnline?'ONLINE':'OFFLINE'} <fa-icon icon="exclamation-triangle"></fa-icon></div>`:''}
+			<div class='header-status' ?hidden=${!this.isOfflineBadge}>
+				<span>${this.isOnline?'ONLINE':'OFFLINE'}</span>
+				<fa-icon icon="exclamation-triangle"></fa-icon>
+			</div>
 			<fa-icon ?hidden=${!this.isLoading} 
 				class="spinner" icon="sync"
 				style="position:absolute"></fa-icon>
